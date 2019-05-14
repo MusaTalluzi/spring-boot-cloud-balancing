645a592305cc1b1a15c2b38a2b8cdd8ce94928e5
Musa Talluzi <mtalluzi@redhat.com>
2019-05-14 11:45:24 -0400

    first commit

---
+++ src/main/java/org/optaplanner/springbootcloudbalancing/SpringBootCloudBalancingApplication.java
@@ -0,0 +1,13 @@
+package org.optaplanner.springbootcloudbalancing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCloudBalancingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCloudBalancingApplication.class, args);
+	}
+
+}
